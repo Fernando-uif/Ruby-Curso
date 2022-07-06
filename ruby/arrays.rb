43dfacec576d8f93dd4b99bb17b13e69497eae3d
fernando-ui <fst-96@hotmail.com>
2022-07-05 20:14:29 -0500

    agregando metodos de arrays

--- ruby/arrays.rb
+++ ruby/arrays.rb
@@ -82,6 +82,68 @@ resultado =  names.fetch(200,'no existe')
 # p arr.last
 # p arr.last(3)
 
-locations = ["mexico", "argentina", "chile", "peru"]
-locations.push("bolivia")
-p locations
+# It's adding the string "bolivia" to the end of the array.
+# It's creating an array of strings.
+# s = ["mexico", "argentina", "chile", "peru"]
+# locations.push("bolivia")
+# p locations
+
+# locations << "Estados unidos"
+# p locations
+
+# locations << "Europa" <<"Asia" << "Africa" << "Oceania"
+# p locations
+
+
+# locations.insert(2, "Australia")
+# p  locations
+
+# arr = ["fernando", "armando", "juan"]
+# p arr
+# algo = arr.pop
+# p algo
+
+# last_items = arr.pop(2)
+# p last_items
+
+# p arr
+# arr.shift
+# arr = ["fernando", "armando", "juan"]
+
+# arr.unshift("fernando21")
+# ELEMENTO =  arr.unshift
+# p arr
+
+# equality and inequality
+# a = [1,2,3,4,5]
+# b = [1,2,3,4,5]
+# c = [3,2,1]
+# d = [1,2,3]
+
+# p a.length == b.length 
+# p a.length == c.length
+# p a.length != d.length
+# p a != c
+
+
+# spaceship operator
+# p 5 <=> 3
+# p 3 <=> 5
+# p 3 <=> 3
+# p [1,2,3] <=> [1,2,3,4]
+# p ['a','b','c'] <=> ['a','b','c','d']
+# p [1,2,3] <=> [1,2,19]
+
+# convert range to array
+# p (1...10).to_a
+
+# .is_a?
+p [1,2,3].is_a?(Array)
+p [1,2,3].is_a?(String)
+p [1,2,3].is_a?(Integer)
+p [1,2,3].is_a?(Float)
+p [1,2,3].is_a?(Hash)
+p [1,2,3].is_a?(Range)
+p [1,2,3].is_a?(Symbol)
+p [1,2,3].is_a?(NilClass)
+p [1,2,3].is_a?(TrueClass)
